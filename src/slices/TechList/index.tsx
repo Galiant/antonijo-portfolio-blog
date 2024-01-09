9875e61990fb4dbfaa0cb36bcf488f9c86089997
Antonijo Galic <antonijodev@gmail.com>
2024-01-10 00:34:25 +0100

    Implementing animation for tech list using gsap

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import Bounded from '@/components/Bounded';
 import Heading from '@/components/Heading';
 import { Content } from '@prismicio/client';
 import { SliceComponentProps } from '@prismicio/react';
-import React from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useEffect, useRef } from 'react';
 import { MdCircle } from 'react-icons/md';
+
+gsap.registerPlugin(ScrollTrigger);
 
 /**
  * Props for `TechList`.
@@ -14,11 +20,48 @@ export type TechListProps = SliceComponentProps<Content.TechListSlice>;
  * Component for "TechList" Slices.
  */
 const TechList = ({ slice }: TechListProps): JSX.Element => {
+  const component = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: component.current,
+          start: 'top bottom',
+          end: 'bottom top',
+          scrub: 4,
+        },
+      });
+
+      timeline.fromTo(
+        '.tech-row',
+        {
+          x: index => {
+            return index % 2 === 0
+              ? gsap.utils.random(600, 400)
+              : gsap.utils.random(-600, -400);
+          },
+        },
+        {
+          x: index => {
+            return index % 2 === 0
+              ? gsap.utils.random(-600, -400)
+              : gsap.utils.random(600, 400);
+          },
+          ease: 'power1.inOut',
+        }
+      );
+    }, component);
+
+    return () => ctx.revert(); // Cleanup
+  });
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className='overflow-hidden'
+      ref={component}
     >
       <Bounded as='div'>
         <Heading size='xl' as='h2' className='mb-8'>
